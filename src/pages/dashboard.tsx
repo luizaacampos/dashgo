57d69c3bc7e182eda3ac8be359359af7406f3566
Luiza Campos <luiza.almcampos@gmail.com>
2021-04-28 15:14:02 -0300

    users and create user pages add

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { Flex, SimpleGrid, Box, Text, theme } from "@chakra-ui/react";
 import dynamic from 'next/dynamic'
+import { ApexOptions } from 'apexcharts'
 
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
@@ -8,7 +9,7 @@ const Chart = dynamic(() => import('react-apexcharts'), {
     ssr: false,
 })
 
-const options = {
+const options: ApexOptions = {
     chart: {
         toolbar: {
             show: false,
